65882bbdda7a1fe6bf6111975cc9f971a3fe8388
janoonaj <jano_alex_es@hotmail.com>
2016-11-18 10:58:26 +0100

    salir de la app si se pulsa back button. Sin anuncio.

--- Assets/SCRIPTS/ADS/StartAppBackPlugin.cs
+++ Assets/SCRIPTS/ADS/StartAppBackPlugin.cs
@@ -4,19 +4,20 @@ using StartApp;
 
 public class StartAppBackPlugin : MonoBehaviour{
 
-	void Start () {
+	/*void Start () {
 	#if UNITY_ANDROID
 		StartAppWrapper.loadAd();
 	#endif
-    }
+    }*/
 
 	void Update () {
 	#if UNITY_ANDROID
 		if (Input.GetKeyUp (KeyCode.Escape)) {
-			if (StartAppWrapper.onBackPressed(gameObject.name) == false) {
+            Application.Quit();
+            /*if (StartAppWrapper.onBackPressed(gameObject.name) == false) {
 				exit();
-			}
-		}
+			}*/
+        }
 	#endif
     }
 	
